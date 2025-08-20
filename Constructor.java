f025e577d657042402f636a8a4af3d053f730b86
Manish Timsina <manishtimsina770319@gmail.com>
2025-08-20 09:42:36 +0545

    feat: add a feature of default constructor

---
+++ Constructor.java
@@ -0,0 +1,25 @@
+public class Constructor {
+    private int age;
+    private String name;
+
+
+    public Constructor(){                   // default constructor
+       // System.out.println("this is the constructor");
+       age = 11;
+       name = "manish";
+    }
+
+    public void display(){
+        System.out.println(age + name);
+    }
+
+    public static void main(String[] args) {
+        Constructor obj1 = new Constructor();
+        obj1.display();
+
+        Constructor obj2 = new Constructor();
+        obj2.display();
+    }
+}
+
+//object create gardai value assign garxa constructor le 
