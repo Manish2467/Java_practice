422f339c8bb5e09087cd6de84f265ad58d5f92b2
Manish Timsina <manishtimsina770319@gmail.com>
2025-08-22 15:41:36 +0545

    prac: practice oop concept polymorphism

---
+++ Polymorphism.java
@@ -0,0 +1,19 @@
+public class Polymorphism {
+    int num1;
+    int num2;
+
+
+    public int add(int n1, int n2){
+        return n1+n2;   
+    }
+
+    public int add(int l1, int l2, int l3){
+        return l1+l2+l3;
+    }
+    public static void main(String[] args) {
+        Polymorphism poly = new Polymorphism();
+
+        System.out.println(poly.add(1,2));
+        System.out.println(poly.add(1,2,3));
+    }
+}
