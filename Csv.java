98c14ad87ff0baa9151fefa0ceba7ab76af06299
Manish2467 <manishtimsina770319@gmail.com>
2025-12-11 12:32:24 +0545

    feat: practice csv reading

--- Csv.java
+++ Csv.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.Exception;
 
 public class Csv {
+    public static void main(String[] args) {
+        String line;
+        String file = "data.csv";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(file))){
+            while((line = br.readLine())!= null){
+                String[] values =  line.split(",");
+                for(String v : values){
+                    System.out.print(v + " ");
+
+
+                }
+                System.out.println();
+
+            }
+        }
+
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
