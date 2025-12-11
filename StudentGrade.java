03f7b2a3345cfaf7fd7cbb72c3c1e15f5229af33
Manish2467 <manishtimsina770319@gmail.com>
2025-12-11 12:26:27 +0545

    feat:practice student grade

--- StudentGrade.java
+++ StudentGrade.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class StudentGrade {
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        String choice;
+        do {
+            System.out.println("enter the marks of the student: ");
+            int marks = sc.nextInt();
+            sc.nextLine();
+            if (marks == 0) {
+                System.out.println("the student is absent");
+            } else {
+                String result = (marks >= 40) ? "pass" : "fail";
+                System.out.println(result);
+
+                String grade = marks >= 85 ? "A" :
+                        marks >= 75 ? "B" :
+                                marks >= 55 ? "C" :
+                                        marks >= 45 ? "D" : "E";
+                System.out.println(grade);
+
+            }
+            System.out.println("do you want to continue:y/n");
+            choice = sc.nextLine();
+
+        } while (choice.equals("y")) ;
+    }
 }
