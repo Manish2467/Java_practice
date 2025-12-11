8ea3865178439c4c1d93568a8b25b652cc7a2040
Manish2467 <manishtimsina770319@gmail.com>
2025-12-11 12:25:45 +0545

    feat: practice ternary operators

--- TernaryOperator.java
+++ TernaryOperator.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TernaryOperator {
+    public static void main() {
+       /* int age = 10;
+        String result = (age > 9) ? "Yes" : "No";
+        System.out.println(result);
+        */
+
+        System.out.println("enter the age to check if he/she is eligible for voting or not: ");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        String result = (age > 18) ? "yes he/she is eligible":"no not eligible";
+        System.out.println(result);
+    }
 }
