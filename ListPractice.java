dcee49f1e9195d1730cd95cb7ca0cc06b4ed4816
Manish2467 <manishtimsina770319@gmail.com>
2025-12-11 12:33:27 +0545

    feat: list practice

--- ListPractice.java
+++ ListPractice.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListPractice {
+    static void main() {
+        List<String> names = new ArrayList<>();
+        for(String name : names){
+            System.out.println(name);
+        }
+        System.out.println("\n");
+
+        names.add("manish");
+        names.add(0,"hari");
+
+        for(String name : names){
+            System.out.println(name);
+        }
+        System.out.println("\n");
+
+        names.set(0,"gaurab");
+        for(String name : names){
+            System.out.println(name);
+        }
+        System.out.println("\n");
+
+        names.remove(0);
+        names.remove("manish");
+        for(String name : names){
+            System.out.println(name);
+        }
+        System.out.println("\n");
+    }
 }
+
+// list is an interface we cannot create object of interface
+//    so arraylist is the class which implements list so we create new object of arraylist
+
