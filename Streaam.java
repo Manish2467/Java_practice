903c3789e3c6bb3919d536e0fb48e1fb26387d0b
Manish2467 <manishtimsina770319@gmail.com>
2025-12-11 12:26:50 +0545

    feat: practice stream

--- Streaam.java
+++ Streaam.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class Streaam {
+    static void main() {
+        List<String> names = List.of("manish", "jenish", "laxu", "mandeep", "manoj");
+
+//        names.stream()
+//                .filter(n -> n.length()>3)
+//                .map(String :: toUpperCase)
+//                .forEach(System.out::println);   //names.forEach(names -> system.out::println(names));
+//    }
+
+        List<String> nam = names.stream()
+                .filter(n -> n.startsWith("m"))
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println(nam);
+    }
 }
+
+
+// stream chai data lai clean sort garna help garxa aafai chai data store gardaina
+// source -> intermediate node -> terminal operation
+//aggregate function le chai stream use garxa
+
+// aggregate functions le chai multiple value bata euta answer dinxa
