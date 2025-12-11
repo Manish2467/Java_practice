773a77e9c8fba80c44907309182988297900fa08
Manish2467 <manishtimsina770319@gmail.com>
2025-12-11 12:31:12 +0545

    feat: practice Even and odd

--- EvenOdd.java
+++ EvenOdd.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class EvenOdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        String choice;
+        do {
+            int a = sc.nextInt();
 
-        if(a%2==0){
-            System.out.println(a+" is even number");
-        }
-        else{
-            System.out.println(a+" is odd number");
-        }
+            if (a % 2 == 0) {
+                System.out.println(a + " is even number");
+            } else {
+                System.out.println(a + " is odd number");
+            }
+            System.out.println("do you want to continue");
+            choice = sc.next();
+        }while(choice.equalsIgnoreCase("Y"));
     }
 }
