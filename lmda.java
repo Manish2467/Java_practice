0ae4719c25fc45bc40a9b23445851df79d3262e5
Manish2467 <manishtimsina770319@gmail.com>
2025-12-11 12:31:58 +0545

    feat : practice lambda

--- lmda.java
+++ lmda.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class lmda {
+    static void main() {
+        Predicate<Integer> isEven = n -> n%2==0;  // yes / no ma answer dinxa
+
+        Supplier<Integer> numberSupplier = () -> 99; // value input linxa output hudaina
+
+        Consumer<Integer> numberConsumer = n -> System.out.println(n); // value out dinxa input hudaina
+
+        int num = numberSupplier.get();
+
+        if (isEven.test(num)){
+            numberConsumer.accept(num);
+        }
+    }
 }
